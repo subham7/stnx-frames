1bdddcc55cbcc8b1922e7e08961bea60a39b4b38
Subham Sengupta <subham@stationx.network>
2024-03-24 21:06:26 +0530

    Server url changed

--- src/config/config.js
+++ src/config/config.js
@@ -22,7 +22,7 @@ if (error) {
 module.exports = {
   env: envVars.NODE_ENV,
   port: envVars.PORT,
-  serverUrl: 'https://stnx-frames-24417d68ca10.herokuapp.com',
+  serverUrl: envVars.SERVER_URL,
   networks: {
     '0x2105': {
       network: 8453,
